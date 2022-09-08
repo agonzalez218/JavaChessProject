4ce4a47bb39429178dbed4b5aeb63caaa06ac042
Abel Gonzalez <fakekiwis@gmail.com>
2022-09-08 12:14:41 -0500

    Created Menu Option and Allowed Board Color Change

--- src/HighlightTiles.java
+++ src/HighlightTiles.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class HighlightTiles extends Board {
 
+    static int option = 68;
+
+    public static void setOption(int newOption){
+        option = newOption;
+    }
+
+    public static int getOption(){
+        return option;
+    }
+
     // Highlights an available spot to move
     public static void highlightSquare(List<JButton> tileList)
     {
@@ -34,6 +44,31 @@ public class HighlightTiles extends Board {
     {
         for (JButton jButton : tileList) {
             jButton.setBorder(BorderFactory.createMatteBorder(3,3,3,3,Color.red));
+        }
+    }
+
+    public static void changeChessBoardBackground()
+    {
+        int i, j;
+        for( i = 0; i < 8; i++)
+        {
+            for( j = 0; j < 8; j++)
+            {
+                if( i % 2 == 0 && j % 2 == 1 || i % 2 == 1 && j % 2 == 0)
+                {
+                    switch (option) {
+                        case 68 -> chessBoard[i][j].setBackground(Color.lightGray);
+                        case 84 -> chessBoard[i][j].setBackground(new Color(111, 78, 55));
+                    }
+                }
+                else
+                {
+                    switch (option) {
+                        case 68 -> chessBoard[i][j].setBackground(Color.white);
+                        case 84 -> chessBoard[i][j].setBackground(new Color(245, 222, 179));
+                    }
+                }
+            }
         }
     }
 }
