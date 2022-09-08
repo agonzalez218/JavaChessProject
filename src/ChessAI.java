4e2873c142e5c63a6c49670ac266a429ce597a82
Abel Gonzalez <fakekiwis@gmail.com>
2022-09-08 16:17:35 -0500

    File Optimization

--- src/ChessAI.java
+++ src/ChessAI.java
@@ -3,35 +3,47 @@ import javax.swing.*;
 public class ChessAI extends Board{
 
     static JButton[][] tempChessBoard = new JButton[8][8];
+    static String originalTile;
+    static String newLocation;
 
-    public static String chooseAIPiece(){
+    public static void setOriginalPiece(String selectedChessPiece){
+        originalTile = selectedChessPiece;
+    }
+
+    public static void setNewLocation(String selectedNewLocation){
+        newLocation = selectedNewLocation;
+    }
+
+    public static String getOriginalPiece(){
 
         String originalPiece;
         originalPiece = chessBoard[1][1].getActionCommand();
         return originalPiece;
     }
 
-    public static int[] chooseAIPieceLocation(){
+    public static int[] getNewLocationArr(){
         int[] arrLocation = new int[2];
-        int x, y;
-        x = 1;
-        y = 3;
-        arrLocation[0] = x;
-        arrLocation[1] = y;
+        arrLocation[0] = (newLocation.charAt(0)-8)*-1;
+        arrLocation[1] = newLocation.charAt(1)-65;
         return arrLocation;
+    }
+
+    public static void test(){
+        setOriginalPiece("7B, bPawn");
+        setNewLocation("5B");
     }
 
     public static void moveAIPiece()
     {
         if(blackTurn)
         {
-            String movingPiece = chooseAIPiece();
+            String movingTile = getOriginalPiece();
 
             // Select original piece
-            if( movingPiece.length() > 2 && !pieceSelected && ((blackTurn && movingPiece.contains("b")) ))
+            if( movingTile.length() > 2 && !pieceSelected && ((blackTurn && movingTile.contains("b")) ))
             {
-                savePiece(movingPiece);
-                int[] newPieceLocation = chooseAIPieceLocation();
+                savePiece(movingTile);
+                int[] newPieceLocation = getNewLocationArr();
                 String newLocation = ((-1*newPieceLocation[1])+8) + String.valueOf((char)(newPieceLocation[0]+65));
                 // Move piece
                 if( teamMoved && pieceSelected && (availableTiles.contains(chessBoard[newPieceLocation[0]][newPieceLocation[1]]) || availableEnemyTiles.contains(chessBoard[newPieceLocation[0]][newPieceLocation[1]])))
